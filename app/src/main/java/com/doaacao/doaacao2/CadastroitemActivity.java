68978377d68bb7fdccb17a49996e523ff138f48b
franklindorea <franklindorea@gmail.com>
2018-12-13 19:32:28 -0300

    Initial commit mazumavez

---
+++ app/src/main/java/com/doaacao/doaacao2/CadastroitemActivity.java
@@ -0,0 +1,13 @@
+package com.doaacao.doaacao2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroitemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastroitem);
+    }
+}
